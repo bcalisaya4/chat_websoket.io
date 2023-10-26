4e6416734f48a1b1628da6c9101cf2e0632de1fa
bcalisaya4 <bcalisaya.veox@gmail.com>
2023-10-26 02:25:15 -0500

    websocket conection

--- server/index.js
+++ server/index.js
@@ -1,15 +1,28 @@
 import express from 'express'
 import logger from 'morgan'
 
+import {Server} from 'socket.io'
+import { createServer } from 'node:http'
+
 const port = process.env.PORT ?? 3000
 
 const app = express()
+const server = createServer(app)
+const io = new Server(server)
+
 app.use(logger('dev'))
 
-app.get('/', (req, res)=>{
-    res.send('<h1>Esto es el chat </h1>')
+io.on('connection', (socket)=>{
+    console.log('a user has connected!')
+    socket.on('disconnect', () =>{
+        console.log('an user has disconnected')
+    })
 })
 
-app.listen(port, ()=>{
+app.get('/', (req, res)=>{
+    res.sendFile(process.cwd() + '/client/index.html')
+})
+
+server.listen(port, ()=>{
     console.log(`server running on port ${port}`)
 })
